f753071a1f9c4bf2a51ece3cb692494b68a2281e
Abdallah Mohamed <160781861+Abdallah229@users.noreply.github.com>
2024-10-25 18:14:51 +0300

    .

--- T3/main.cpp
+++ T3/main.cpp
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-#include "Memory.h"
-#include "Regist.h"
-=======
 #include "Memory.h" 
 #include "Regist.h" 
->>>>>>> 0d3b5eebc2b29b2198bb145eff065963e41fccca
+
 int main ()
 {
-    cout << "Yarab N7las Mn el Ramly " << endl; //hatetshaf fel history keda
+    cout << "Yarab N7las" << endl; //hatetshaf fel history keda 😂😂😂 
 }
