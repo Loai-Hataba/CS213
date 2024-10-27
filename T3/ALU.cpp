52ea0fd76119988c61db3a8b76eca9259085dc61
Ahmed Abdelaziz <saulgoodman2005@gmail.com>
2024-10-28 00:48:47 +0300

    added add dandadan

--- T3/ALU.cpp
+++ T3/ALU.cpp
@@ -1,34 +1,21 @@
 #include "ALU.h"
-#include "Regist.h"
 #include "Headers.h"
 
-//string ALU::hexToDec() {
-//    if (!isValid(hexStr)) {
-//        cout<<"buh"<<endl;
-//    }
-//
-//    int decimalValue = stoi(hexStr, nullptr, 16);
-//    return to_string(decimalValue);
-//}
-//
-//string ALU::decToHex() {
-//        int decimalValue = stoi(decStr);
-//        string hexStr;
-//
-//        while (decimalValue > 0) {
-//            int remainder = decimalValue % 16;
-//            char hexChar = (remainder < 10) ? '0' + remainder : 'A' + (remainder - 10);
-//            hexStr = hexChar + hexStr;
-//            decimalValue /= 16;
-//        }
-//
-//        return hexStr.empty() ? "0" : hexStr;
-//}
 
+ void ALU::add(string idx1, string idx2, string idx3, Register& reg) {
+     int val1 = hexToDec(reg.getCell(idx2));
+     int val2 = hexToDec(reg.getCell(idx3));
+     while (val2 != 0) {
+         int carry = val1 & val2;
+         val1 = val1 ^ val2;
+         val2 = carry << 1;
+     }
+     string res = decToHex(val1);
+     reg.setCell(idx1, res);
+ }
 
-// void ALU::add(string idx1, string idx2, string idx3, Register& reg) {
-//     string val1 = reg.getCell(idx1);
-//     string val2 = reg.getCell(idx2);
-//     int res = val1 + val2;
-//     reg.setCell(idx3, res);
-// }
+ void ALU::floatingAdd(string idx1, string idx2, string idx3, Register& reg){
+     double val1 = hexToDec(reg.getCell(idx2));
+     double val2 = hexToDec(reg.getCell(idx3));
+
+}
--- T3/ALU.h
+++ T3/ALU.h
@@ -1,7 +1,9 @@
 #include "Headers.h"
 #include "Regist.h"
+#include "Methods.h"
 
 class ALU {
     public:
         void add(string idx1, string idx2, string idx3, Register& reg);
+        void floatingAdd(string idx1, string idx2, string idx3, Register& reg);
 };
