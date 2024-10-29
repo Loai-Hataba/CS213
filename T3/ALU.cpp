9d227a708abcf7b00ca7b0660630f32cc5de5f83
Ahmed Abdelaziz <saulgoodman2005@gmail.com>
2024-10-29 03:02:25 +0300

    workin on floatin add yuh

--- T3/ALU.cpp
+++ T3/ALU.cpp
@@ -3,37 +3,21 @@
 
 
 void ALU::add(string idx1, string idx2, string idx3, Register& reg) {
-
-    // Convert hexadecimal to binary for binary addition
     string bin1 = hexToBin(reg.getCell(idx2));
     string bin2 = hexToBin(reg.getCell(idx3));
 
-    // Perform binary addition
-    string result_bin;
-    int carry = 0;
-    int len = max(bin1.size(), bin2.size());
+    string res = addBin(bin1, bin2);
 
-    // Extend both binary strings to the same length with sign extension
-    string b1 = string(len - bin1.size(), bin1[0]) + bin1;
-    string b2 = string(len - bin2.size(), bin2[0]) + bin2;
-    // Add binary strings from the last bit to the first
-    for (int i = len - 1; i >= 0; --i) {
-        int bit1 = b1[i] - '0';
-        int bit2 = b2[i] - '0';
-        int sum = bit1 + bit2 + carry;
-        result_bin = char((sum % 2) + '0') + result_bin;
-        carry = sum / 2;
-    }
-
-    if (carry == 1) {
-        result_bin = "1"+result_bin;
-    }
-    string res = binToHex(result_bin);
     reg.setCell(idx1, res);
 }
 
-void ALU::floatingAdd(string idx1, string idx2, string idx3, Register& reg){
-    double val1 = hexToDec(reg.getCell(idx2));
-    double val2 = hexToDec(reg.getCell(idx3));
-
+void ALU::floatingAdd(string idx1, string idx2, string idx3, Register& reg){ //not yet cuh
+    string bin1 = hexToBin(reg.getCell(idx2));
+    string bin2 = hexToBin(reg.getCell(idx3));
+    string sign1 = bin1.substr(0,1);
+    string sign2 = bin2.substr(0,1);
+    string exp1 = bin1.substr(1,3);
+    string exp2 = bin2.substr(1,3);
+    string man1 = bin1.substr(4);
+    string man2 = bin2.substr(4);
 }
