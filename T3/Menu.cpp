2ccb9947bd520362b2b8cd26bb7ecf012b282595
Abdallah Mohamed <160781861+Abdallah229@users.noreply.github.com>
2024-10-29 23:44:19 +0300

    Fixed Extract Instructions

--- T3/Menu.cpp
+++ T3/Menu.cpp
@@ -1,5 +1,12 @@
 #include "Menu.h"
 #include <regex>
+bool CheckInst(string Inst)
+{
+    if (!isdigit(Inst[0]) && (Inst[0] != 'B' || Inst[0] != 'C'))
+        return false;
+
+    return true;
+}
 
 string GetFile()
 {
@@ -9,33 +16,22 @@ string GetFile()
     {
         string Path = GetPath();
         ifstream File(Path); // Open the file using ifstream
-        if (File.fail())
-        {
-            // Check if the file failed to open
-            cout << "Unable to open the file --> (" << Path << "). Please choose another one with (.txt) format." << endl;
+        if (File.is_open())
+        { // Check if the file failed to open
+            cout << "The File --> ( " << Path << " ) Opened Successfully " << endl; // Debugging line
+            stringstream Content;
+            Content << File.rdbuf(); // Read the entire file content
+            Result = Content.str();  // Store the content in a string
+            File.close();            // Close the file
+            break;
         }
         else
-        {
-            cout << "The file --> (" << Path << ") opened successfully." << endl;
-
-            // Read file content character by character
-            char ch;
-            while (File.get(ch)) // File.get() reads one character at a time
-            {
-                // Append only printable ASCII characters (from space to ~)
-                if (ch >= 32 && ch <= 126)
-                {
-                    Result += ch;
-                }
-            }
-
-            File.close(); // Close the file
-            break;
+        {                                                                           // File opened successfully
+            cout << "Unable To Open The File --> ( " << Path << " ) . Please Choose Another One With (.txt) Format" << endl;
         }
     }
     return Result; // Return the file content
 }
-
 vector<string> ExtractInst(string Content)
 {
     vector<string> inst;
@@ -43,14 +39,17 @@ vector<string> ExtractInst(string Content)
     for (size_t i = 0; i < Content.size(); i++)
     {
         // Corrected the condition for alpha to ensure it checks both ranges properly
-        bool alpha = isdigit(Content[i]) || (Content[i] >= 'B' && Content[i] <= 'C');
+        bool alpha = isdigit(Content[i]) || (Content[i] >= 'A' && Content[i] <= 'F');
         cout << Content[i] << endl;
         // If temp has 4 characters, push them as two separate hex pairs
         if (temp.size() == 4)
         {
-            inst.push_back(temp.substr(0, 2));
-            inst.push_back(temp.substr(2, 2));
-            temp.clear();
+            if (CheckInst)
+            {
+                inst.push_back(temp.substr(0, 2));
+                inst.push_back(temp.substr(2, 2));
+                temp.clear();
+            }
         }
 
         // If the character is a space and temp size is less than 4 or if the character is not valid, clear temp
@@ -63,10 +62,6 @@ vector<string> ExtractInst(string Content)
         // If the character is valid, add it to temp
         if (alpha)
         {
-            if(Content[i] == '7' ) continue;
-            if(Content[i] == '8' ) continue;
-            if(Content[i] == '9' ) continue;
-
             temp += Content[i];
         }
     }
@@ -77,16 +72,15 @@ vector<string> ExtractInst(string Content)
         inst.push_back(temp.substr(0, 2));
         inst.push_back(temp.substr(2, 2));
     }
-   cout << endl; 
+    cout << endl;
     return inst;
 }
 
 int main()
 {
-    string Content  = GetFile () ;
-    vector <string> Instructions  = ExtractInst (Content);
+    string Content = GetFile();
+    vector<string> Instructions = ExtractInst(Content);
     Machine machine(Instructions);
-    
 
-    return 0 ;
+    return 0;
 }
