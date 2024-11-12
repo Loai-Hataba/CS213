0f7cc095375796922baa0a8be12c50098c36923e
Abdallah Mohamed <160781861+Abdallah229@users.noreply.github.com>
2024-11-12 13:26:37 +0200

    Close To Finish problem 2

--- A2/Problem 2.cpp
+++ A2/Problem 2.cpp
@@ -2,180 +2,145 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include <sstream>
 #include <cmath>
-using namespace std ;
-vector<string> parseString(const string &str,  vector<string> set , bool Remove = false ) ;
-bool IsInSet(string str, vector<string> S);
+using namespace std;
 
-class StringSet
-{
-    vector <string> Set ;
-    public :
-    StringSet() ; // Empty Constructor
-    StringSet (string str) ; // Parametrized Constructor
-    void addString(string str) ;
-    void removeString (string str) ;
+// Function declarations
+vector<string> parseString(const string &str, vector<string> set, bool Remove = false);
+bool IsInSet(const string& str, const vector<string>& S);
+
+class StringSet {
+    vector<string> Set;
+public:
+    StringSet(); // Empty Constructor
+    StringSet(const string& input, int type = 2); // Constructor with input type
+    void addString(const string& str);
+    void removeString(const string& str);
     void clearSet() { Set.clear(); }
-    void outputSet() ;
-    StringSet operator + (StringSet s) ;
-    StringSet operator * (StringSet s) ;
-    void computeSimilarity (StringSet s) ;
-    vector<string> getSet() {return Set; }
-    void setSet(vector<string> S) {Set = S ;}
-    void PushElement(string str) {Set.push_back(str);}
+    void outputSet() const;
+    StringSet operator+(const StringSet& s);
+    StringSet operator*(const StringSet& s);
+    void computeSimilarity(const StringSet& s);
+    vector<string> getSet() const { return Set; }
+    void setTokens(const vector<string>& S) { Set = S; }
+    void PushElement(const string& str) { Set.push_back(str); }
 };
-int main()
-{
 
-    StringSet s1("IAM UNstoppable and You are nothing ") , s2("Iam UNstoppable and You are nothing ");
-    s1.outputSet() ;
-    s1.computeSimilarity(s2) ;
-    StringSet s3 = s1*s2 ;
-    s3.outputSet() ;
-    return 0 ;
-
-}
-//////////////////////////////////////////////////////////////////////////////////////
-StringSet::StringSet(string str) // Parametrized Constructor
-{
-   setSet( parseString(str, this -> getSet() )) ;
-}
-StringSet::StringSet()
-{
-    setSet(vector<string> (0 , "")) ;
-}
-/////////////////////////////////////////////////////////////////////////////////////////////
-void StringSet::addString(string str)
-{
-setSet(parseString(str, this -> getSet())) ;
-}
-//////////////////////////////////////////////////////////////////////////////////////
-void StringSet::removeString(string str)
-{
-    setSet(parseString(str, this -> getSet() , true  )) ;
+// Main function
+int main() {
+    StringSet s1("IAM UNstoppable and You are nothing " ), s2("Iam Abdallah and You are nothing ");
+    s1.outputSet();
+    s1.computeSimilarity(s2);
+    StringSet s3 = s1 + s2;
+    s3.outputSet();
+    StringSet s4 ;
+    s4.outputSet();
+    return 0;
 }
 
-//////////////////////////////////////////////////////////////////////////////////////
-void StringSet::outputSet()
-{
-    cout << "The Set  :  { " ;
-    for (size_t i = 0; i < Set.size(); i++)
-    {
-        if(i != Set.size() - 1)
-        {
-            cout << Set[i] << " , " ;
+// Empty Constructor
+StringSet::StringSet() {
+    Set.clear();
+}
+
+// Constructor with input type indicator (1 for file, 2 for string)
+StringSet::StringSet(const string& input, int type) {
+    if (type == 1) { // File input
+        ifstream file(input);
+        if (file) {
+            stringstream ss;
+            ss << file.rdbuf();
+            setTokens( parseString(ss.str(), vector<string>()) );
+        } else {
+            cerr << "Error opening file: " << input << endl;
         }
-        else
-        {
-            cout << Set[i] << " } " ;
+    } else { // String input
+        setTokens(parseString(input, vector<string>()));
+    }
+}
+
+void StringSet::addString(const string& str) {
+    Set = parseString(str, this->getSet());
+}
+
+void StringSet::removeString(const string& str) {
+    Set = parseString(str, this->getSet(), true);
+}
+
+void StringSet::outputSet() const {
+    cout << "The Set: { ";
+    for (size_t i = 0; i < Set.size(); ++i) {
+        cout << Set[i];
+        if (i != Set.size() - 1) {
+            cout << ", ";
         }
     }
-    cout << endl;
-
-
+    cout << " }" << endl;
 }
 
-StringSet StringSet::operator+( StringSet  s)
-{
-    StringSet res = *this ;
-   for(auto val : s.getSet() )
-   {
-       if(IsInSet(val , res.getSet())) {
-
-       };
-   }
-   return res;
-
-}
-
-StringSet StringSet::operator*(StringSet s)
-{
+StringSet StringSet::operator+(const StringSet& s) {
     StringSet res = *this;
-    vector<string> tokens = res.getSet();
-    vector<string> intersection;  // To store the intersection of the two sets
-
-    // Iterate through each element of the current set and check if it exists in the other set
-    for (auto val : tokens)
-    {
-        if (IsInSet(val, s.getSet()))  // Check if val is in s.getSet()
-        {
-            intersection.push_back(val);  // If yes, add to the intersection
+    for (const auto& val : s.getSet()) {
+        if (!IsInSet(val, res.getSet())) {
+            res.PushElement(val);
         }
     }
-
-    res.setSet(intersection);  // Set the new set as the intersection
     return res;
 }
 
-void StringSet::computeSimilarity(StringSet s)
-{
-    vector<string> setQ = this->getSet();  // Get the current set
-    vector<string> setD = s.getSet();      // Get the other set
+StringSet StringSet::operator*(const StringSet& s) {
+    StringSet res;
+    vector<string> intersection;
+    for (const auto& val : Set) {
+        if (IsInSet(val, s.getSet())) {
+            intersection.push_back(val);
+        }
+    }
+    res.setTokens(intersection);
+    return res;
+}
+
+void StringSet::computeSimilarity(const StringSet& s) {
+    vector<string> setQ = this->getSet();
+    vector<string> setD = s.getSet();
     int intersectionSize = 0;
-    // Calculate the size of the intersection of the two sets
-    for (const string& word : setQ)
-    {
-        if (IsInSet(word, setD))  // Check if word is in both sets
-        {
+    for (const string& word : setQ) {
+        if (IsInSet(word, setD)) {
             intersectionSize++;
         }
     }
-    // Calculate the similarity
     int Q = setQ.size();
     int D = setD.size();
-   cout <<  (intersectionSize) / (sqrt(Q) * sqrt(D)) << endl;
+    double similarity = (Q > 0 && D > 0) ? (intersectionSize / (sqrt(Q) * sqrt(D))) : 0;
+    cout << "Similarity: " << similarity << endl;
 }
 
-
-////////////////////////////////////////////////////////////////////////////
-vector<string> parseString(const string &str, vector<string> set, bool Remove)
-{
+vector<string> parseString(const string &str, vector<string> set, bool Remove) {
     vector<string> res = set;
     string temp;
-
-    for (char ch : str)
-    {
-        if (isalpha(ch))
-        {
+    for (char ch : str) {
+        if (isalpha(ch)) {
             temp += tolower(ch);
-        }
-        else if (!temp.empty())
-        {
-            if (!IsInSet(temp, res))
-            {
+        } else if (!temp.empty()) {
+            if (!IsInSet(temp, res)) {
                 res.push_back(temp);
-            }
-            else if (Remove)
-            {
-                // Find and erase the element if it exists
+            } else if (Remove) {
                 auto it = find(res.begin(), res.end(), temp);
-                if (it != res.end())
-                {
+                if (it != res.end()) {
                     res.erase(it);
                 }
             }
             temp.clear();
         }
     }
-
-    // Check the final temp after the loop ends
-    if (!temp.empty() && !IsInSet(temp, res))
-    {
+    if (!temp.empty() && !IsInSet(temp, res)) {
         res.push_back(temp);
     }
-
     return res;
 }
 
-//////////////////////////////////////////////////////////////////////////////////////
-bool IsInSet(string str, vector<string> S)
-{
-    for (size_t i = 0; i < S.size(); i++)
-    {
-        if (S[i] == str)
-            return true;
-    }
-
-    return false;
+bool IsInSet(const string& str, const vector<string>& S) {
+    return find(S.begin(), S.end(), str) != S.end();
 }
-//////////////////////////////////////////////////////////////////////////////////////
